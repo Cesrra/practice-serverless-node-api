f7c712e2b67dcbc409d4927c74ded2bf310f6666
cesrra <c3z4r.rincon@gmail.com>
2024-07-01 06:56:34 -0500

    Congfigure the postgres and used the neon from neondatabase/serverless to make instance of sql and use the sql sentences

--- src/handler.js
+++ src/handler.js
@@ -1,10 +1,20 @@
 const serverless = require("serverless-http");
 const express = require("express");
 const app = express();
+const { neon, neonConfig } = require('@neondatabase/serverless')
 
-app.get("/", (req, res, next) => {
+const dbClient = async () => {
+  neonConfig.fetchConnectionCache = true
+  const sql = neon(process.env.DATABASE_URL)
+  return sql
+}
+
+app.get("/", async (req, res, next) => {
+  const sql = await dbClient()
+  const [results] = await sql`select now()`
   return res.status(200).json({
-    message: "Hello from root!",
+    message: "Hello from root! ",
+    results: results.now
   });
 });
 
@@ -24,7 +34,19 @@ app.use((req, res, next) => {
 // app.listen(3000, () => {
 //   console.log('Server - Listening on port 3000')
 // })
-//Intall Serverless Offline -> npm install --save-dev serverless-offline@latest
-  //Run --> serverless offline
+/*
+Intall Serverless Offline -> npm install --save-dev serverless-offline@latest
+  Run --> serverless offline
+.ENV plugin
+  install -> npm install --save-dev serverless-dotenv-plugin@latest
+neonctl -> is used to conect with serverless database neon
+neonctl auth -> to give the permisions on the neon project
+neonctl connection-string -> give the conection string of the database
+  sudo apt install postgresql-client
+  psql 'postgresql://neondb_owner:26MUwSGcCfpA@ep-winter-art-a5fzhjak.us-east-2.aws.neon.tech/neondb?sslmode=require'
+neonctl branches list
+neonctl branches create --name name_branche
+neonctl connection-string --branch dev
+*/
 
 exports.handler = serverless(app);
